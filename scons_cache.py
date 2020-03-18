4d52d91152266d7b2098cd6bcf0c1f2408998951
Shane Smiskol <shane@smiskol.com>
2020-03-17 22:15:20 -0500

    quicker

--- scons_cache.py
+++ scons_cache.py
@@ -6,5 +6,5 @@ env = os.environ.copy()
 env['SCONS_CACHE'] = "1"
 
 nproc = os.cpu_count()
-j_flag = "" if nproc is None else "-j%d" % (nproc - 1)
-scons = subprocess.Popen(["scons", j_flag], cwd=BASEDIR, env=env, stderr=subprocess.PIPE)
+j_flag = '-j16'
+scons = subprocess.Popen(["scons", j_flag], cwd=BASEDIR, env=env)
